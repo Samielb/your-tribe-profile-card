4999d8a08f89fd36c30608f86377548f4b013975
Samielb <samibaghdadi15@hotmail.com>
2026-09-02 09:09:36 +0200

    Update
    Heb mijn visitekaartje kwa style wat mooier gemaakt door de kleur meer donkerder te maken en heb een Taal knop toegevoeg om van Engels naar nederlands te kunnen switchen

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,32 @@
 /* Stap 1: Selecteer de knop */
-let expandButton = document.querySelector('button')
+let expandButton = document.querySelector('.expand-button')
 
 /* Stap 2: Wacht tot iemand op die knop klikt */
 expandButton.addEventListener('click', function() {
 
   /* Stap 3: Verander een class zodra iemand op die knop klikt */
   document.body.classList.toggle('is-expanded')
+})
+
+let languageButton = document.querySelector('.language-button')
+let slogan = document.querySelector('.slogan em')
+let aboutMe = document.querySelector('.about-me')
+let isEnglish = false
+
+languageButton.addEventListener('click', function() {
+  isEnglish = !isEnglish
+
+  if (isEnglish) {
+    slogan.textContent = 'Junior Front-end Developer'
+    aboutMe.innerHTML = 'About me <br>I am an eager Junior Front-end Developer with experience in HTML, CSS, PHP and JavaScript. I enjoy building modern and user-friendly websites.'
+    languageButton.textContent = 'NL'
+    languageButton.setAttribute('aria-label', 'Verander taal naar Nederlands')
+    document.documentElement.lang = 'en'
+  } else {
+    slogan.textContent = 'Junior Front end Developer'
+    aboutMe.innerHTML = 'About me <br>Ik ben een leergierige Junior Front-end Developer met ervaring in HTML, CSS, PHP en JavaScript. Ik bouw graag moderne en gebruiksvriendelijke websites.'
+    languageButton.textContent = 'EN'
+    languageButton.setAttribute('aria-label', 'Verander taal naar Engels')
+    document.documentElement.lang = 'nl'
+  }
 })
